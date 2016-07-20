674a4028a443ca83e91ac1f6f740e9f347c801b9
Rs43863 <ranjit.s@zensar.in>
2016-07-20 17:49:37 +0530

    Extra code Added

--- WebContent/calculatorPage.jsp
+++ WebContent/calculatorPage.jsp
@@ -1,25 +1,34 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
-<html> <h1>
+<html> <h1 align="center">
 Welcome to our Devops Training !!
 </h1>
-        <body>
-                <form method="post" action="CalculatorController">
-                        Number 1 : <input type="text" name="no1"> <br>
-                        Number 2 : <input type="text" name="no2"> <br> 
+        <body> <div align="center">
+                <form  name="calculatorForm" method="post" action="CalculatorController">
+                        Number 1 : <input type="text" name="firstNo" value="${result}${firstNo}"> <br>
+                        Number 2 : <input type="text" name="secondNo" value="${secondNo}"> <br> 
                         Operator : 
                         
-                        <select name="opt">
+                        <select name="opt" onchange="document.calculatorForm.submit()">
+	                        	<option value="">Select Operation</option>	
                                 <option value="plus"> + </option>
                                 <option value="minus"> - </option>
                                 <option value="multiply"> * </option>
                                 <option value="divide"> / </option>
-                                                                                                                                
+                                <option value="percentage"> % </option>
+                                <option value="square"> x*x </option>
+                                <option value="inverse"> 1/x </option>
+								<option value="squareRoot"> _/x </option>                                                                                                                                
                         </select>
                                 
                         <br>
-                        Result : <label>${sessionScope.result}</label> <br>
-                        <input type="submit" value="Go">
+                        <c:if test="${not empty result}">
+                        Result : <label>${result}</label> <br>
+                        </c:if>
+                        	<c:if test="${not empty errorMessage}">
+                        	 <label>${errorMessage}</label><br>
+                        	</c:if>	
                 </form>
+                </div>
         </body>
 </html>
--- src/com/zensar/simplecalculatordevops/controllers/CalculatorController.java
+++ src/com/zensar/simplecalculatordevops/controllers/CalculatorController.java
@@ -1,11 +1,15 @@
-package com.zensar.calculatordevops.controller;
+package com.zensar.simplecalculatordevops.controllers;
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import com.zensar.simplecalculatordevops.services.SimpleCalculatorDevopsServices;
+import com.zensar.simplecalculatordevops.services.SimpleCalculatorDevopsServicesImpl;
 
 /**
  * Servlet implementation class CalculatorController
@@ -14,17 +18,33 @@ import javax.servlet.http.HttpSession;
 public class CalculatorController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     /*public CalculatorController() { super();  }*/
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		 String n1 = request.getParameter("no1");
-         String n2 = request.getParameter("no2");
-         String opt = request.getParameter("opt");
-         HttpSession session = request.getSession();
-         if(opt.equals("plus"))
-                 session.setAttribute("result", Integer.parseInt(n1) + Integer.parseInt(n2));
-         else if(opt.equals("minus"))
-        	     session.setAttribute("result", Integer.parseInt(n1) - Integer.parseInt(n2));
-         request.getRequestDispatcher("calculatorPage.jsp").forward(request, response);
+		SimpleCalculatorDevopsServices services = new SimpleCalculatorDevopsServicesImpl() ;
+		 Double firstNo=(Double)request.getAttribute("firstNo");
+		 Double secondNo=(Double)request.getAttribute("secondNo");
+		 String opt = request.getParameter("opt");
+         /*HttpSession session = request.getSession();*/
+         if(opt.equals("plus")){
+        	 request.setAttribute("result", services.add(firstNo ,secondNo));
+         }else if(opt.equals("minus")){
+        	 request.setAttribute("result", services.minus(firstNo ,secondNo));
+         }else if(opt.equals("multiply")){
+        	 request.setAttribute("result", services.multiply(firstNo ,secondNo));
+         }else if(opt.equals("divide")){
+        	 request.setAttribute("result", services.divide(firstNo ,secondNo));
+         }else if(opt.equals("percentage")){
+        	 request.setAttribute("result", services.percentage(firstNo ,secondNo));
+         }else if(opt.equals("square")){
+        	 request.setAttribute("result", services.square(firstNo));
+         }else if(opt.equals("inverse")){
+        	 request.setAttribute("result", services.inverse(firstNo));
+         }else if(opt.equals("squareRoot")){
+        	 request.setAttribute("result", services.squareRoot(firstNo));
+         }
+         request.removeAttribute("firstNo");request.removeAttribute("secondNo");
+        
+        request.getRequestDispatcher("calculatorPage.jsp").forward(request, response);
 	}
 
 }
